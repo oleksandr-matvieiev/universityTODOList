579d50e894b8b7147c39efd2221b31fdd7c59830
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-01-29 21:38:49 +0100

    added missed required annotation

--- src/main/java/org/example/universitytodolist/security/CustomUserDetailsService.java
+++ src/main/java/org/example/universitytodolist/security/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import org.example.universitytodolist.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
