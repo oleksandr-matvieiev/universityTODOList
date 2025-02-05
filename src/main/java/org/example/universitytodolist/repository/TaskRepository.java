ae7726956f0318f22ca9b2197f0906df0877492f
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-02-05 13:27:27 +0100

    made reminder for task deadlines

--- src/main/java/org/example/universitytodolist/repository/TaskRepository.java
+++ src/main/java/org/example/universitytodolist/repository/TaskRepository.java
@@ -6,6 +6,7 @@ import org.example.universitytodolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllBySubject_IdAndUser_Id(Long subjectId, Long userId);
 
     List<Task> findAllByStatusAndUser(TaskStatus status, User user);
+
+    List<Task> findAllByDeadLineBetween(LocalDateTime from, LocalDateTime to);
 }
---
+++ src/main/java/org/example/universitytodolist/service/TaskReminderService.java
@@ -0,0 +1,40 @@
+package org.example.universitytodolist.service;
+
+import org.example.universitytodolist.model.Task;
+import org.example.universitytodolist.model.User;
+import org.example.universitytodolist.repository.TaskRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TaskReminderService {
+    private final TaskRepository taskRepository;
+    private final EmailService emailService;
+
+
+    public TaskReminderService(TaskRepository taskRepository, EmailService emailService) {
+        this.taskRepository = taskRepository;
+        this.emailService = emailService;
+    }
+
+    //    @Scheduled(cron = "0 0 10 * * ?")
+    @Scheduled(fixedRate = 5000)
+    public void reminder() {
+        LocalDateTime treeDayLater = LocalDateTime.now().plusDays(3);
+        List<Task> tasks = taskRepository.findAllByDeadLineBetween(LocalDateTime.now(), treeDayLater);
+
+        for (Task task : tasks) {
+            User user = task.getUser();
+            String subject = "\uD83D\uDD14 Reminder about deadline:" + task.getTitle();
+            String message = "Hi, " + user.getUsername() + "!<br><br>" +
+                    "We would like to inform you that the deadline for the task<b>" + task.getTitle() + "</b>" +
+                    " is approaching. You have <b>3 more days</b> to complete it.<br><br>" +
+                    "<i>Sincerely, UniversityToDoList</i>";
+            emailService.sendEmail(user.getEmail(), subject, message);
+        }
+        System.out.println("Reminder completed");
+    }
+}
