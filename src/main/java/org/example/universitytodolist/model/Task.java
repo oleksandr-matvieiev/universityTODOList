85aa434552d232402c9f794b9bcc63f7425a8b9e
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-02-05 19:52:47 +0100

    updated task model

--- src/main/java/org/example/universitytodolist/model/Task.java
+++ src/main/java/org/example/universitytodolist/model/Task.java
@@ -3,6 +3,7 @@ package org.example.universitytodolist.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,8 +22,10 @@ public class Task {
     private Long id;
 
     @Column(nullable = false)
+    @Size(min = 1, max = 50)
     private String title;
 
+    @Size(min = 1, max = 512)
     private String description;
 
     private LocalDateTime deadLine;
@@ -50,10 +53,10 @@ public class Task {
     @JoinColumn(name = "gradebook_id")
     private GradeBook gradeBook;
 
-    @OneToMany(mappedBy = "task")
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
 
-    @OneToOne(mappedBy = "task")
+    @OneToOne(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     private Notification notification;
 }
 
