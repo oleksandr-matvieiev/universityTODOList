e7520b8a5b7297b7be08cd01eb703cbd8db443f5
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-02-11 15:37:40 +0100

    added required import

--- frontend/src/component/TaskPage.js
+++ frontend/src/component/TaskPage.js
@@ -3,6 +3,7 @@ import {useParams, useNavigate} from "react-router-dom";
 import axios from "axios";
 
 import "./TaskPage.css";
+import NavigationMenu from "./NavigationMenu";
 
 const TaskPage = () => {
     const {subjectName} = useParams();
